058f110347a15361cc70b101f33c1545c36bb320
BaconEggsRL <apatton090@gmail.com>
2025-01-24 17:27:09 -0500

    key2

--- main.gd
+++ main.gd
@@ -5,7 +5,19 @@ const min_fade_samples:float = 100
 const max_fade_samples:float = mix_rate * 4
 @export_range (min_fade_samples, max_fade_samples, 1) var fade_samples = (min_fade_samples + max_fade_samples) / 2
 
-@export var record:Button
+@export var record_btn:Button
+@export var play_btn:Button
+
+@export var save_btn:Button
+@export var status:Label
+var save_dir:String = "user://"
+
+var record_test = "Record"
+var stop_text = "Stop"
+var effect:AudioEffectRecord
+var recording:AudioStreamWAV
+
+
 
 @export var release_knob:Knob
 
@@ -46,39 +58,25 @@ var piano_buttons:Array
 
 
 
-func _on_piano_button_pressed(btn:PianoButton) -> void:
-	print("hello")
-	note_label.text = btn.text
+
+
 	
-	
-func _on_knob_turned(deg:float, type:String="") -> void:
-	match type:
-		"A":
-			pass
-		"S":
-			pass
-		"D":
-			pass
-		"R":
-			# min_fade_samples
-			# max_fade_samples
-			# fade_samples
-			# release_knob.rotation_degrees = deg
-			var clamped_deg = clamp(deg, 18.1690, 81.8309)
-			var value = remap(clamped_deg, 18.1690, 81.8309, min_fade_samples, max_fade_samples)
-			print(value)
-			self.fade_samples = value
-			for btn:PianoButton in piano_buttons:
-				btn.fade_samples = self.fade_samples
-			
-			pass
-		_:
-			push_warning("'%s' type not matched" % type)
-	pass
-
-
-
 func _ready() -> void:
+	# record button
+	# We get the index of the "Record" bus.
+	var idx = AudioServer.get_bus_index("Master")
+	# And use it to retrieve its first effect, which has been defined
+	# as an "AudioEffectRecord" resource.
+	effect = AudioServer.get_bus_effect(idx, 0)
+	record_btn.text = record_test
+	self.record_btn.toggled.connect(_on_record_toggled)
+	self.play_btn.toggled.connect(_on_play_toggled)
+	if not recording:
+		play_btn.disabled = true
+	self.save_btn.pressed.connect(_on_save_pressed)
+		
+	
+	# release knob
 	release_knob.turned.connect(_on_knob_turned.bind("R"))
 	var knob_init = remap(fade_samples, min_fade_samples, max_fade_samples, 18.1690, 81.8309)
 	var fang :float= lerp_angle( release_knob.knob.rotation, knob_init, 0.3  )
@@ -186,6 +184,87 @@ func process_event(event) -> void:
 	
 
 
+
+func _on_save_pressed():
+	var save_path = save_dir + "test.wav"
+	recording.save_to_wav(save_path)
+	status.text = "Saved WAV file to: \"%s\"\n(%s)" % [save_path, ProjectSettings.globalize_path(save_path)]
+	
+	
+	
+func _on_play_toggled(_toggled_on: bool) -> void:
+	if recording:
+		play_btn.disabled = true
+		
+		print(recording)
+		print(recording.format)
+		print(recording.mix_rate)
+		print(recording.stereo)
+		var data = recording.get_data()
+		print(data.size())
+		audio_player.stream = recording
+		
+		audio_player.finished.connect((func(): play_btn.disabled = false), CONNECT_ONE_SHOT)
+		audio_player.play()
+		
+		pass
+	
+	
+	
+func _on_record_toggled(_toggled_on: bool) -> void:
+	if effect.is_recording_active():
+		record_btn.text = record_test
+		recording = effect.get_recording()
+		effect.set_recording_active(false)
+		
+		play_btn.disabled = false
+		# $SaveButton.disabled = false
+		
+		# $RecordButton.text = "Record"
+		# $Status.text = ""
+	else:
+		record_btn.text = stop_text
+		effect.set_recording_active(true)
+		
+		play_btn.disabled = true
+		# $SaveButton.disabled = true
+		
+		# $RecordButton.text = "Stop"
+		# $Status.text = "Recording..."
+
+
+func _on_piano_button_pressed(btn:PianoButton) -> void:
+	print("hello")
+	note_label.text = btn.text
+	
+	
+func _on_knob_turned(deg:float, type:String="") -> void:
+	match type:
+		"A":
+			pass
+		"S":
+			pass
+		"D":
+			pass
+		"R":
+			# min_fade_samples
+			# max_fade_samples
+			# fade_samples
+			# release_knob.rotation_degrees = deg
+			var clamped_deg = clamp(deg, 18.1690, 81.8309)
+			var value = remap(clamped_deg, 18.1690, 81.8309, min_fade_samples, max_fade_samples)
+			print(value)
+			self.fade_samples = value
+			for btn:PianoButton in piano_buttons:
+				btn.fade_samples = self.fade_samples
+			
+			pass
+		_:
+			push_warning("'%s' type not matched" % type)
+	pass
+	
+	
+	
 # func _on_piano_key_toggled(_toggled_on:bool, _btn:PianoButton) -> void:
 	# pass
 	# update button pressed
